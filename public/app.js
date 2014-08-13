d4ab618a05091f2a0b2e85cf564a9950bb3b9b25
darrenmwong <darrenwong06@gmail.com>
2014-08-13 02:14:58 -0700

    added sounds

--- public/app.js
+++ public/app.js
@@ -1,3 +1,20 @@
+
+
+
+
+
+//Sounds Used
+
+var volume = 30;
+var soundJump = new buzz.sound("assets/sounds/sfx_wing.ogg");
+var soundScore = new buzz.sound("assets/sounds/sfx_point.ogg");
+var soundHit = new buzz.sound("assets/sounds/sfx_hit.ogg");
+var soundDie = new buzz.sound("assets/sounds/sfx_die.ogg");
+var soundSwoosh = new buzz.sound("assets/sounds/sfx_swooshing.ogg");
+buzz.all().setVolume(volume);
+
+
+//Init Document
 $(document).ready(function() {
   $("#testing").click(function() {
     console.log('this');
