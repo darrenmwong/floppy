0d490e5807cc7b32ec3727d02d2d359b9b624c9f
darrenmwong <darrenwong06@gmail.com>
2014-08-13 02:18:35 -0700

    added variables for stae/gravity/scores/pipes

--- public/app.js
+++ public/app.js
@@ -1,3 +1,27 @@
+//States of game
+
+var states = Object.freeze({
+   SplashScreen: 0,
+   GameScreen: 1,
+   ScoreScreen: 2
+});
+
+var currentstate;
+
+var gravity = 0.25;
+var velocity = 0;
+var position = 180;
+var rotation = 0;
+var jump = -4.6;
+
+var score = 0;
+var highscore = 0;
+
+var pipeheight = 90;
+var pipewidth = 52;
+var pipes = new Array();
+
+var replayclickable = false;
 
 
 
@@ -16,7 +40,10 @@ buzz.all().setVolume(volume);
 
 //Init Document
 $(document).ready(function() {
-  $("#testing").click(function() {
-    console.log('this');
-  });
+    console.log('Ready')
+
+    //Start Splash Screen
+    showSplash();
+
+
 });
